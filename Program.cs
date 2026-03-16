6611e708f4c303b9ede50d9c434a255e9b75206b
Manu <Manu@DESKTOP-KE8ND2T>
2026-03-16 17:13:54 +0200

    modificat structura

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿ // Asigură-te că pui numele corect al proiectului tău
-using Proiect_PIU.enums;
+﻿using Proiect_PIU.enums;
 using Proiect_PIU.models;
 using System;
 
@@ -11,22 +10,17 @@ namespace Proiect_PIU
         {
             Console.WriteLine("=== Testare Sistem Gestiune ITP (Lab 2) ===\n");
 
-            // 1. Creăm un proprietar
             Proprietar proprietar1 = new Proprietar("Ion Popescu", "1900101123456", "0722123456");
 
-            // 2. Creăm o mașină asociată acestui proprietar
             Masina masina1 = new Masina("Dacia", "Logan", 2018, "B-123-ABC", proprietar1);
 
-            // 3. Creăm un inspector
             Inspector inspector1 = new Inspector("Vasile Ionescu", "RO-ITP-456");
 
-            // 4. Simulăm o inspecție ITP cu rezultat ADMIS
             InspectieITP inspectie1 = new InspectieITP(DateTime.Now, Rezultat.Admis, inspector1);
 
-            // Adăugăm inspecția în istoricul mașinii
             masina1.IstoricInspectii.Add(inspectie1);
 
-            // 5. Afișăm datele pentru a testa că totul s-a legat corect
+            //afisare date pentru a testa ca totul a fost creat corect si legat corect
             Console.WriteLine($"Proprietar: {masina1.Proprietar.Nume}");
             Console.WriteLine($"Masina: {masina1.Marca} {masina1.Model} ({masina1.NrInmatriculare})");
             Console.WriteLine($"Data Inspectiei: {inspectie1.DataEfectuare.ToShortDateString()}");
