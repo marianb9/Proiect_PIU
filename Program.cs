3da3e558b6108f310e4844ecfee0f1cfb85b07e6
Manu <Manu@DESKTOP-KE8ND2T>
2026-03-19 18:00:37 +0200

    Rezolvat T.A. lab3 adaugaret ManagerMasini.cs si meniu interactiv consola

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-﻿using Proiect_PIU.enums;
+﻿using System;
 using Proiect_PIU.models;
-using System;
+using Proiect_PIU.manager;
 
 namespace Proiect_PIU
 {
@@ -8,28 +8,106 @@ namespace Proiect_PIU
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("=== Testare Sistem Gestiune ITP (Lab 2) ===\n");
+            ManagerMasini manager = new ManagerMasini();
 
-            Proprietar proprietar1 = new Proprietar("Ion Popescu", "1900101123456", "0722123456");
+            while (true)
+            {
+                Console.WriteLine("\n=== SISTEM GESTIUNE ITP ===");
+                Console.WriteLine("1. Adauga o masina noua (Citire de la tastatura)");
+                Console.WriteLine("2. Afiseaza toate masinile inregistrate");
+                Console.WriteLine("3. Cauta o masina dupa numarul de inmatriculare");
+                Console.WriteLine("0. Iesire");
+                Console.Write("Alege o optiune: ");
 
-            Masina masina1 = new Masina("Dacia", "Logan", 2018, "B-123-ABC", proprietar1);
+                string optiune = Console.ReadLine();
 
-            Inspector inspector1 = new Inspector("Vasile Ionescu", "RO-ITP-456");
+                switch (optiune)
+                {
+                    case "1":
+                        CitireMasinaDeLaTastatura(manager);
+                        break;
+                    case "2":
+                        AfisareMasini(manager);
+                        break;
+                    case "3":
+                        CautaMasina(manager);
+                        break;
+                    case "0":
+                        Console.WriteLine("Se inchide aplicatia...");
+                        return;
+                    default:
+                        Console.WriteLine("Optiune invalida! Incearca din nou.");
+                        break;
+                }
+            }
+        }
 
-            InspectieITP inspectie1 = new InspectieITP(DateTime.Now, Rezultat.Admis, inspector1);
+        static void CitireMasinaDeLaTastatura(ManagerMasini manager)
+        {
+            Console.WriteLine("\n--- INTRODUCERE DATE PROPRIETAR ---");
+            Console.Write("Nume Proprietar: ");
+            string nume = Console.ReadLine();
+            Console.Write("CNP: ");
+            string cnp = Console.ReadLine();
+            Console.Write("Telefon: ");
+            string telefon = Console.ReadLine();
 
-            masina1.IstoricInspectii.Add(inspectie1);
+            Proprietar proprietarNou = new Proprietar(nume, cnp, telefon);
 
-            //afisare date pentru a testa ca totul a fost creat corect si legat corect
-            Console.WriteLine($"Proprietar: {masina1.Proprietar.Nume}");
-            Console.WriteLine($"Masina: {masina1.Marca} {masina1.Model} ({masina1.NrInmatriculare})");
-            Console.WriteLine($"Data Inspectiei: {inspectie1.DataEfectuare.ToShortDateString()}");
-            Console.WriteLine($"Rezultat ITP: {inspectie1.Rezultat}");
-            Console.WriteLine($"Inspector responsabil: {inspectie1.InspectorCareAAprobat.Nume}");
-            Console.WriteLine($"Data Expirare ITP: {inspectie1.DataExpirare.ToShortDateString()}");
+            Console.WriteLine("\n--- INTRODUCERE DATE MASINA ---");
+            Console.Write("Marca: ");
+            string marca = Console.ReadLine();
+            Console.Write("Model: ");
+            string model = Console.ReadLine();
 
-            Console.WriteLine("\napasati orice tasta pentru a inchide...");
-            Console.ReadKey();
+            Console.Write("An fabricatie: ");
+            int an;
+            while (!int.TryParse(Console.ReadLine(), out an))
+            {
+                Console.Write("An invalid! Introdu un numar: ");
+            }
+
+            Console.Write("Numar inmatriculare (ex: B-12-ABC): ");
+            string nrInmat = Console.ReadLine();
+
+            Masina masinaNoua = new Masina(marca, model, an, nrInmat, proprietarNou);
+
+            manager.AdaugaMasina(masinaNoua);
+            Console.WriteLine("Masina a fost salvata cu succes!");
+        }
+
+        static void AfisareMasini(ManagerMasini manager)
+        {
+            var masini = manager.GetToateMasinile();
+
+            if (masini.Count == 0)
+            {
+                Console.WriteLine("\nNu exista nicio masina inregistrata in sistem.");
+                return;
+            }
+
+            Console.WriteLine("\n--- LISTA MASINI ---");
+            foreach (var m in masini)
+            {
+                Console.WriteLine($"> Masina: {m.Marca} {m.Model} | An: {m.AnFabricatie} | Nr: {m.NrInmatriculare} | Proprietar: {m.Proprietar.Nume}");
+            }
+        }
+
+        static void CautaMasina(ManagerMasini manager)
+        {
+            Console.Write("\nIntrodu numarul de inmatriculare cautat: ");
+            string nrCautat = Console.ReadLine();
+
+            Masina masinaGasita = manager.CautaDupaNrInmatriculare(nrCautat);
+
+            if (masinaGasita != null)
+            {
+                Console.WriteLine($"\nMASINA GASITA: {masinaGasita.Marca} {masinaGasita.Model}, an {masinaGasita.AnFabricatie}, apartine lui {masinaGasita.Proprietar.Nume} ({masinaGasita.Proprietar.Telefon}).");
+            }
+            else
+            {
+                Console.WriteLine("\nMasina cu acest numar de inmatriculare nu a fost gasita in sistem.");
+            }
         }
     }
 }
---
+++ manager/ManagerMasini.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Proiect_PIU.models;
+
+namespace Proiect_PIU.manager
+{
+    public class ManagerMasini
+    {
+        private List<Masina> listaMasini = new List<Masina>();
+
+        public void AdaugaMasina(Masina masina)
+        {
+            listaMasini.Add(masina);
+        }
+
+        public List<Masina> GetToateMasinile()
+        {
+            return listaMasini;
+        }
+
+        public Masina CautaDupaNrInmatriculare(string nrInmatriculare)
+        {
+            foreach (Masina m in listaMasini)
+            {
+                if (m.NrInmatriculare.ToUpper() == nrInmatriculare.ToUpper())
+                {
+                    return m;
+                }
+            }
+            return null;
+        }
+    }
+}
