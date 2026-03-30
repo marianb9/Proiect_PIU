5443108b6caacd13720f6ad881729d5795c64e83
Manu <Manu@DESKTOP-KE8ND2T>
2026-03-30 12:37:35 +0300

    test

--- Program.cs
+++ Program.cs
@@ -41,7 +41,7 @@ namespace Proiect_PIU
                 }
             }
         }
-                       // Metoda pentru citirea datelor unei masini de la tastatura si salvarea acesteia in fisier
+                       // Metoda pentru citirea datelor unei masini de la tastatura si salvarea acesteia in fisier//test
         static void CitireMasinaDeLaTastatura(ManagerMasiniFisier manager)
         {
             Console.WriteLine("\n--- INTRODUCERE DATE PROPRIETAR ---");
